fd72617e314cc4255566f2e9c598f213e708672b
DeusNuntio <deusnuntio@googlemail.com>
2026-07-05 16:26:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // an respondWith übergeben (führte zu TypeError); stattdessen kontrollierte
 // 503/504-Antworten.
 
-const CACHE = 'nadigpfau-v172';
+const CACHE = 'nadigpfau-v175';
 
 self.addEventListener('install', () => {
   // Sofort aktivieren, nicht auf alten SW warten
