1cac79f7d1ed26264440a417d59c2a8d12d2f039
DeusNuntio <deusnuntio@googlemail.com>
2026-07-13 16:03:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // an respondWith übergeben (führte zu TypeError); stattdessen kontrollierte
 // 503/504-Antworten.
 
-const CACHE = 'nadigpfau-v212';
+const CACHE = 'nadigpfau-v213';
 
 // v195 (B4): Kern-Assets beim Install vorab cachen, damit die App auch nach
 // einer Browser-Cache-Raeumung offline startet (bisher nur On-the-fly-Cache).
