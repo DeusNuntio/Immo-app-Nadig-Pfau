dab03f6e8df745ade787c184f850012184a11dff
DeusNuntio <deusnuntio@googlemail.com>
2026-08-06 22:29:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
    ───────────────────────────────────────────────────────────────────────── */
 'use strict';
 
-const CACHE = 'nadigpfau-v322';
+const CACHE = 'nadigpfau-v323';
 
 const CORE = [
   './',
